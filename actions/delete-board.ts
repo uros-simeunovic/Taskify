85ec4377bee6e5d7155bf4383a5d82645e6e6e55
uros-simeunovic <dixypixy007@gmail.com>
2024-04-12 14:51:10 +0200

    Boards

---
+++ actions/delete-board.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export async function deleteBoard(id: string) {
+  await db.board.delete({ where: { id } });
+
+  revalidatePath("/organization/org_2eldAAliXB2XmK9N5ywWKsDAHXO");
+}
