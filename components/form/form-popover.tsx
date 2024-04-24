d8be0896fbcf0d6b3eee25490b9632aac85b3b89
uros-simeunovic <dixypixy007@gmail.com>
2024-04-24 11:39:37 +0200

    stripe subscription

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -16,6 +16,7 @@ import { toast } from "sonner";
 import { FormPicker } from "./form-picker";
 import { ElementRef, useRef } from "react";
 import { useRouter } from "next/navigation";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -30,6 +31,7 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const proModal = useProModal();
   const router = useRouter();
   const closeRef = useRef<ElementRef<"button">>(null);
 
@@ -40,8 +42,8 @@ export const FormPopover = ({
       router.push(`/board/${data.id}`);
     },
     onError: (error) => {
-      console.log({ error });
       toast.error(error);
+      proModal.onOpen();
     },
   });
 
